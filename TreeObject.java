9fcf1c7e772cc724b53491982e0608a89dd66bcc
thomasreinkingBSU <47871176+thomasreinkingBSU@users.noreply.github.com>
2019-04-22 17:41:24 -0600

    Implements TreeObject

--- TreeObject.java
+++ TreeObject.java
@@ -1,6 +1,43 @@
+/**
+ * Creates the tree object that contains the key and freq
+ * @author thomasreinking
+ *
+ */
 public class TreeObject {
 	 
-	public TreeObject() {
-		//TODO
+	private int freq;
+	private long key;
+	
+	/**
+	 * Creates a tree object with a given frequency and key
+	 * Could be used for when a duplicate is found.
+	 */
+	public TreeObject(long key, int freq) {
+		this.freq = freq;
+		this.key = key;
+	}
+	
+	/**
+	 * Creates a tree object with only the key
+	 * Used for when no duplicates is found
+	 * @param key
+	 */
+	public TreeObject(long key) {
+		this.key = key;
+		freq = 1;
+	}
+	
+	/**
+	 * Increases the freq count 
+	 */
+	public void increaseFreq() {
+		freq++;
+	}
+	
+	/**
+	 * Returns the freq of the object
+	 */
+	public int getFreq() {
+		return freq;
 	}
 }
